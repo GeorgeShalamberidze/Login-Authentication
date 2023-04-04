c5702e3cb1f94d76ed7eb3bfd5f23ea0420565dd
George Shalamberidze <giorgishalamberidze1995@gmail.com>
2023-04-04 04:47:12 +0400

    Update generateToken.js

--- backend/src/generateToken.js
+++ backend/src/generateToken.js
@@ -3,17 +3,25 @@ const jwt = require("jsonwebtoken");
 const generateAccessToken = (user) => {
   const { username, id } = user;
 
-  return jwt.sign({ username, id }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "15m",
-  });
+  return jwt.sign(
+    { username, id },
+    process.env.ACCESS_TOKEN_SECRET || "your secret key",
+    {
+      expiresIn: "15m",
+    }
+  );
 };
 
 const generateRefreshToken = (user) => {
   const { username, id } = user;
 
-  return jwt.sign({ username, id }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: 604800, // 7 days
-  });
+  return jwt.sign(
+    { username, id },
+    process.env.REFRESH_TOKEN_SECRET || "your secret key",
+    {
+      expiresIn: 604800, // 7 days
+    }
+  );
 };
 
 module.exports = { generateAccessToken, generateRefreshToken };
